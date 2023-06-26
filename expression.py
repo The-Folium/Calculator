3c7c933558aebaac85b5bad214f508e1c6c638ac
Alex <alex.buriakovsky@gmail.com>
2023-06-26 08:24:15 +0300

    Log10 issue

--- expression.py
+++ expression.py
@@ -5,13 +5,9 @@ class Expression:
         self.line = line
 
     def evaluate(self):
-        # for function in ['sin', 'cos', 'tan', 'asin', 'acos', 'atan',
-        #                  'sqrt', 'log', 'exp', 'pi']:
-        #
-        #     if function_pos:=self.line.find(function) != -1:
-        #
-        #         line = self.line
-        #         self.line = line[:function_pos-1]+'math.'+line[function_pos-1:]
+        while function_pos:=self.line.find("log(") != -1:
+            line = self.line
+            self.line = line[:function_pos+2]+'10'+line[function_pos+2:]
 
         try:
             result = str(round(eval(self.line), 12))
